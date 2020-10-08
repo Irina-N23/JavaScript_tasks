5729a095e0bb02b06160ed6e7e332706be39fcf6
Irina Novik <solvejg.23@gmail.com>
2020-10-08 12:26:14 +0300

    Improve code after review

--- DataTypes12573.js
+++ DataTypes12573.js
@@ -1,32 +1,34 @@
-let givenString = "JavaScript";
-let givenBoolean = true;
+"use strict";
+
+const GIVEN_STRING = "JavaScript";
+const GIVEN_BOOLEAN = true;
 
 // 1) boolean/string:
-alert(givenBoolean + givenString); // "trueJavaScript"
-alert(givenBoolean * givenString); // NaN
-alert(givenBoolean / givenString); // NaN
-alert(givenBoolean - givenString); // NaN
+alert(GIVEN_BOOLEAN + GIVEN_STRING); // "trueJavaScript"
+alert(GIVEN_BOOLEAN * GIVEN_STRING); // NaN
+alert(GIVEN_BOOLEAN / GIVEN_STRING); // NaN
+alert(GIVEN_BOOLEAN - GIVEN_STRING); // NaN
 
-alert(Boolean(givenString)); // true
-alert(Number(givenString)); // NaN
+alert(Boolean(GIVEN_STRING)); // true
+alert(Number(GIVEN_STRING)); // NaN
 
-alert(String(givenBoolean)); // "true"
-alert(Number(givenBoolean)); // 1
+alert(String(GIVEN_BOOLEAN)); // "true"
+alert(Number(GIVEN_BOOLEAN)); // 1
 
 
 // 2) string/string:
-let newString = "hello ";
+const NEW_STRING = "hello ";
 
-alert(newString + givenString); // "hello JavaScript"
-alert(" 5 " + givenString); // " 5 JavaScript"
-alert(newString * givenString); // NaN
+alert(NEW_STRING + GIVEN_STRING); // "hello JavaScript"
+alert(" 5 " + GIVEN_STRING); // " 5 JavaScript"
+alert(NEW_STRING * GIVEN_STRING); // NaN
 alert(" 003  " * "2"); // 6
-alert(newString / givenString); // NaN
+alert(NEW_STRING / GIVEN_STRING); // NaN
 alert("96" / "8"); // 12
-alert(newString - givenString); // NaN
+alert(NEW_STRING - GIVEN_STRING); // NaN
 
-alert(Number(newString)); // NaN
-alert(Boolean(newString)); // true
+alert(Number(NEW_STRING)); // NaN
+alert(Boolean(NEW_STRING)); // true
 
 alert(Number(" 0152  ")); // 152 
 alert(Boolean(" 0152  ")); // true
@@ -34,46 +36,46 @@ alert(Boolean("")); // false
 
 
 // 3) string/number:
-let givenNumber = 200;
+const GIVEN_NUMBER = 200;
 
-alert(givenString + givenNumber); // "JavaScript200"
-alert(givenString * givenNumber); // NaN
-alert(givenString / givenNumber); // NaN
-alert(givenString - givenNumber); // NaN
+alert(GIVEN_STRING + GIVEN_NUMBER); // "JavaScript200"
+alert(GIVEN_STRING * GIVEN_NUMBER); // NaN
+alert(GIVEN_STRING / GIVEN_NUMBER); // NaN
+alert(GIVEN_STRING - GIVEN_NUMBER); // NaN
 
-alert(Number(givenString)); // NaN
-alert(Boolean(givenString)); // true
+alert(Number(GIVEN_STRING)); // NaN
+alert(Boolean(GIVEN_STRING)); // true
 alert(Boolean("")); // false
 
-alert(String(givenNumber)); // "200"
-alert(Boolean(givenNumber)); // true
+alert(String(GIVEN_NUMBER)); // "200"
+alert(Boolean(GIVEN_NUMBER)); // true
 
 
 // 4) number/string:
-alert(givenNumber + givenString); // "200JavaScript"
-alert(givenNumber * givenString); // NaN
-alert(givenNumber / givenString); // NaN
-alert(givenNumber - givenString); // NaN
+alert(GIVEN_NUMBER + GIVEN_STRING); // "200JavaScript"
+alert(GIVEN_NUMBER * GIVEN_STRING); // NaN
+alert(GIVEN_NUMBER / GIVEN_STRING); // NaN
+alert(GIVEN_NUMBER - GIVEN_STRING); // NaN
 
-alert(String(givenNumber)); // "200"
-alert(Boolean(givenNumber)); // true
+alert(String(GIVEN_NUMBER)); // "200"
+alert(Boolean(GIVEN_NUMBER)); // true
 alert(Boolean(0)); // false
 
-alert(Number(givenString)); // NaN
-alert(Boolean(givenString)); // true
+alert(Number(GIVEN_STRING)); // NaN
+alert(Boolean(GIVEN_STRING)); // true
 alert(Boolean('')); // false
 
 
 // 5) number/boolean:
-alert(givenNumber + givenBoolean); // 201
-alert(givenNumber * givenBoolean); // 200
-alert(givenNumber / givenBoolean); // 200
-alert(givenNumber - givenBoolean); // 199
+alert(GIVEN_NUMBER + GIVEN_BOOLEAN); // 201
+alert(GIVEN_NUMBER * GIVEN_BOOLEAN); // 200
+alert(GIVEN_NUMBER / GIVEN_BOOLEAN); // 200
+alert(GIVEN_NUMBER - GIVEN_BOOLEAN); // 199
 
-alert(String(givenNumber)); // "200"
-alert(Boolean(givenNumber)); // true
+alert(String(GIVEN_NUMBER)); // "200"
+alert(Boolean(GIVEN_NUMBER)); // true
 alert(Boolean(0)); // false
 
-alert(Number(givenBoolean)); // 1
+alert(Number(GIVEN_BOOLEAN)); // 1
 alert(Number(false)); // 0
-alert(String(givenBoolean)); // "true"
+alert(String(GIVEN_BOOLEAN)); // "true"
