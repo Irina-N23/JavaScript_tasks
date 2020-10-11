a65dbdfc485faa562dd3f70664e3f60fb0079c8a
Irina Novik <solvejg.23@gmail.com>
2020-10-11 21:12:55 +0300

    fix (functions 12586): add task from 'learn.javascript.ru'

--- functions/functions12586.js
+++ functions/functions12586.js
@@ -63,3 +63,29 @@ function getArrayOfNumbersWithEvenPosition() {
 }
 
 alert(getArrayOfNumbersWithEvenPosition());
+
+
+
+/*
+Замените код Function Expression стрелочной функцией:
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+*/
+
+function ask(question, yes, no) {
+    (confirm(question)) ? yes() : no();
+  }
+
+ask(
+    "Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert('Вы отменили выполнение.')
+);
