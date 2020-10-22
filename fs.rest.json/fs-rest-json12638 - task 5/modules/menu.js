cf40b41c009e9e50268b2cdfc1d871958df58ee2
Irina Novik <solvejg.23@gmail.com>
2020-10-22 12:27:18 +0300

    fix (fs, rest, json 12638 - task 5): correct name of variable

--- fs.rest.json/fs-rest-json12638 - task 5/modules/menu.js
+++ fs.rest.json/fs-rest-json12638 - task 5/modules/menu.js
@@ -2,15 +2,15 @@
 
 const functions = require("./core functionality");
 
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 // Reads a name of inputted command in order to run the appropriqate action:
 const goToChosenAction = () => {
-    const action = arguments[0].toLowerCase();
+    const action = args[0].toLowerCase();
 
     switch(action) {
         case "add":
-            functions.add(arguments[1], arguments[2]);
+            functions.add(args[1], args[2]);
             console.log("\nDone:\tthe note has been added to the list.\n");
         break;
         case "list":
@@ -18,10 +18,10 @@ const goToChosenAction = () => {
             functions.list();
         break;
         case "read":
-            functions.read(arguments[1]);
+            functions.read(args[1]);
         break;
         case "remove":
-            functions.remove(arguments[1]);
+            functions.remove(args[1]);
         break;
         default:
             console.log(`\n(!) Incorrect name of action has been inputted: `
